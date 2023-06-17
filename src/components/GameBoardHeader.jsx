53c2fd3da01323b5fed61d46dfc894ac61ea76f1
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-06-17 13:04:18 +0530

    ADD 🎧 : head part of the main game board

---
+++ src/components/GameBoardHeader.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/logo.svg";
+
+function GameBoardHeader() {
+  return (
+    <div className="w-[632px] flex items-center justify-between mx-auto mb-16">
+      <button className="bg-purple2 text-white font-bold py-1 px-4 rounded-3xl h-10">
+        MENU
+      </button>
+
+      <img src={logo} />
+
+      <button className="bg-purple2 text-white font-bold py-1 px-4 rounded-3xl h-10">
+        RESTART
+      </button>
+    </div>
+  );
+}
+
+export default GameBoardHeader;
